44116b408265cf4b9bc52f5bd5016f331450ef5b
udhay <udhayakumar0405@outlook.com>
2020-11-01 03:41:30 +0000

    1.11.2020

--- structure/basic_struct.c
+++ structure/basic_struct.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 
+struct devices 
+{
+   int  device_no;
+   char *device_name[25];
+};
+
 int main(void)
 {
-    
+    struct devices d1;
+    d1.device_no = 121;
+    d1.device_name = "harry";
+    printf("device-id = %d deviceName = %s\n", d1.device_no, d1.device_name);
 }
