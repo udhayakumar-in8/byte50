c207f29d17e5849dbcc9217000add28e7705299f
root <root@localhost.localdomain>
2020-11-02 06:52:55 +0000

    12.clk-2.11.20220

--- structure/structure.c
+++ structure/structure.c
@@ -3,17 +3,17 @@
 
 struct devices 
 {
-   int  device_no;
-   char device_name[25];
-   int *p;
+    int device_no;
+    char device_name[25];
+    int *p;
 };
 
 int main(void)
 {
     struct devices d1;
     d1.device_no = 121;
-    strcpy(d1.device_name,"harry");
-    d1->p=&d1.device_no;
+    strcpy(d1.device_name,  "harry");
+    d1.p = &d1.device_no;
     printf("device-id = %d deviceName = %s\n", d1.device_no, d1.device_name);
-    printf("device-pointer=%p\n",d1.p );
+    printf("device-pointer = %d\n", *d1.p);
 }
