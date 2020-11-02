1e161936646786ea486d4169025e802a2d7088d9
root <root@localhost.localdomain>
2020-11-02 04:49:46 +0000

    02.11.2020

--- structure/basic_struct.c
+++ structure/basic_struct.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
+#include<string.h>
 
 struct devices 
 {
    int  device_no;
-   char *device_name[25];
+   char device_name[25];
+   int *p;
 };
 
 int main(void)
 {
     struct devices d1;
     d1.device_no = 121;
-    d1.device_name = "harry";
+    strcpy(d1.device_name,"harry");
+    d1->p=&d1.device_no;
     printf("device-id = %d deviceName = %s\n", d1.device_no, d1.device_name);
+    printf("device-pointer=%p\n",d1.p );
 }
