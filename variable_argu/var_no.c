295588a8a0b304417fdb3a5a2696cce72c4aebd3
root <root@localhost.localdomain>
2020-11-05 05:51:51 +0000

    morning-05-04-2020

---
+++ variable_argu/var_no.c
@@ -0,0 +1,28 @@
+/* C program to demonstrate working of variable arguments to find average 
+ of multiple numbers. ref:https://www.eskimo.com/~scs/cclass/int/sx11b.html*/
+#include <stdarg.h> 
+/* This header file is required in any file which uses the variable 
+argument list (va_) macros. */
+#include <stdio.h> 
+  
+int average(int num, ...) 
+{ 
+    va_list valist; 
+  
+    int sum = 0, i; 
+  
+    va_start(valist, num); 
+    for (i = 0; i < num; i++)  
+        sum += va_arg(valist, int); 
+  
+    va_end(valist); 
+  
+    return sum / num; 
+} 
+  
+int main() 
+{ 
+    printf("Average of {2, 3, 4} = %d\n",  average(2, 3, 4)); 
+    printf("Average of {3, 5, 10, 15} = %d\n", average(3, 5, 10, 15)); 
+    return 0; 
+} 
