99719f8512b19afb0d240edaa9fc26092bc87672
udhayakumar-hub <udhayakumar0405@outlook.com>
2020-11-15 13:07:13 +0000

    sunday-commit

---
+++ cmake/c-make/build/CMakeFiles/simple.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/simple"
+  "../bin/simple.pdb"
+  "CMakeFiles/simple.dir/simple.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
