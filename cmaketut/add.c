076cec31e78205fb9120acb54ac899d0e407b723
udhay <udhayakumar0405@outlook.com>
2020-12-02 10:42:40 +0530

    newcommit-dec2

---
+++ cmaketut/add.c
@@ -0,0 +1,4 @@
+float add(float num1, float num2)
+{
+	return num1+num2;
+}
---
+++ cmaketut/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+float add(float,float);
+
+int main(void)
+{
+	printf("this is main function\n");
+	printf("main function completed\n");
+	printf("float function return_value %f \n", add(1.3, 4.7));
+	printf("oops! program completed!\n");
+}
