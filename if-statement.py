0eeadff90b75a1aa57cd1482c895ffac27d47ee5
udhay <udhayakumar0405@outlook.com>
2021-05-12 20:44:34 +0530

    may

---
+++ if-statement.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+i = 0
+if i :
+  print("condition is true")
+print("program completed!")
+     
---
+++ objective_c/normal-input.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%*s %d", &a);
+    printf("Input value read : a=%d",a);
+    return 0;
+}
